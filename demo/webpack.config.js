3cc0fc8cf9072c882967326f20772a36dafa1751
huozhi <gilesliu12@gmail.com>
2016-10-02 03:22:10 +0800

    add demo deploy scripts

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -2,17 +2,18 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const joinPath = path.join.bind(null, __dirname)
+const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
   entry: {
     app: [joinPath('index.js')],
   },
   output: {
-    publicPath: '/',
+    publicPath: isProduction ? '/respinner' : '/',
     path: joinPath('dist'),
-    filename: 'app.js',
+    filename: isProduction ? 'app.[hash].js' : 'app.js',
   },
-  devtool: 'cheap-eval-source-map',
+  devtool: !isProduction && 'cheap-eval-source-map',
   module: {
     loaders: [
       {
@@ -30,10 +31,23 @@ module.exports = {
     require('postcss-import')({addDependencyTo: webpack}),
     require('postcss-nested'),
   ],
-  plugins: [
+  plugins: ([
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+  ]).concat(isProduction ? [
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {warnings: false},
+      output: {comments: false},
+    }),
+    new HtmlWebpackPlugin({
+      template: joinPath('index.html'),
+      minify: false,
+    }),
+  ] : [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
-  ],
+  ]),
   devServer: {
     hot: true,
     inline: true,
