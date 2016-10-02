78284d669190deeac5a6be29a3f1bb2f770b31f7
huozhi <inbox@huozhi.im>
2016-10-02 18:04:26 +0800

    add readme and code example

---
+++ README.md
@@ -0,0 +1,48 @@
+# Respinner
+> react components for spinners or loaders. Each loader was combined css animation and render dom as svg.
+easily controlled by react porps.
+
+### Examples
+
+website: [huozhi.github.io/respinner/](https://huozhi.github.io/respinner/)
+
+### Usage
+
+**4** kinds of loaders supported yer, just import it when use with react.
+
+```js
+import {
+  BeatLoader, CircularLoader, RotateLoader, BounceLoader
+} from 'respinner'
+
+// ...
+
+render() {
+  <div className="spinners">
+    <BeatLoader fill="#333" count={3} />
+    <CircularLoader stroke="#666" />
+  </div>
+}
+```
+
+See more details in `demo` directory.
+
+### Setup
+
+```sh
+npm install
+```
+
+### Development
+
+```sh
+npm start
+
+# see demo in http://localhost:3000
+```
+
+### Build
+
+```
+npm run build
+```
--- demo/index.js
+++ demo/index.js
@@ -6,10 +6,19 @@ import './demo.css'
 const color = '#4197ff'
 
 const loaders = [
-  (<BeatLoader fill={color} />),
-  (<CircularLoader stroke={color} />),
-  (<BounceLoader fill={color} />),
-  (<RotateLoader stroke={color} />),
+  {
+    code: `<BeatLoader fill="${color}" />`,
+    node: (<BeatLoader fill={color} />),
+  }, {
+    code: `<CircularLoader stroke="${color}" />`,
+    node: (<CircularLoader stroke={color} />),
+  }, {
+    code: `<BounceLoader fill="${color}" />`,
+    node: (<BounceLoader fill={color} />),
+  }, {
+    code: `<RotateLoader stroke="${color}" />`,
+    node: (<RotateLoader stroke={color} />),
+  }
 ]
 
 const App = () => (
@@ -17,10 +26,11 @@ const App = () => (
     <div className="App-title">
       <h1>React SVG Spinners</h1>
     </div>
-    <div className="App-demo">
-      {loaders.map((loader, idx) => (
-        <div key={`loader-${idx}`} className="App-loader">
-          {loader}
+    <div className="App-container">
+      {loaders.map(({node, code}, idx) => (
+        <div key={`loader-${idx}`} className="App-demo">
+          <div className="App-loader">{node}</div>
+          <code className="App-code">{code}</code>
         </div>
       ))}
     </div>
