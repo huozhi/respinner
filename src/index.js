3e92c04484996837fff70c469b25141204bfa467
huozhi <gilesliu12@gmail.com>
2017-04-08 21:01:51 +0800

    migrate postcss to inline minified style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-throw Error('import/require from respinner/lib')
+// throw Error('import/require from respinner/lib')
+export {default as BeatLoader} from './components/BeatLoader'
+export {default as BounceLoader} from './components/BounceLoader'
+export {default as CircularLoader} from './components/CircularLoader'
+export {default as ClockLoader} from './components/ClockLoader'
+export {default as RotateLoader} from './components/RotateLoader'
+export {default as SpinLoader} from './components/SpinLoader'
+export {default as WaveLoader} from './components/WaveLoader'
