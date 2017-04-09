b6cb073ff75a55e2133424dc90715587f20d3a62
huozhi <inbox@huozhi.im>
2017-04-09 10:14:17 +0800

    deploy docs in prod env

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -42,6 +42,9 @@ module.exports = {
       }
     })
   ]).concat(isProduction ? [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {warnings: false},
       output: {comments: false},
