cdb52c25d5a13c0a3ebffc05859b05473a6bbbda
huozhi <inbox@huozhi.im>
2019-08-04 18:44:29 +0800

    enhance: module types declare

---
+++ types.d.ts
@@ -0,0 +1,11 @@
+import {FunctionComponent} from 'react'
+
+export const BeatLoading : FunctionComponent; 
+export const BounceLoading : FunctionComponent; 
+export const CircularLoading : FunctionComponent; 
+export const ClockLoading : FunctionComponent; 
+export const RotateLoading : FunctionComponent; 
+export const SpinLoading : FunctionComponent; 
+export const WaveLoading : FunctionComponent; 
+export const DashLoading : FunctionComponent; 
+export const CopperLoading : FunctionComponent; 
