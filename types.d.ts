3c3453496f2feea5990b5dd51945e77febb7e7a6
Jiachi Liu <inbox@huozhi.im>
2022-01-16 14:10:21 +0100

    Fix exports sugar path and compelete typing

--- types.d.ts
+++ types.d.ts
@@ -1,11 +1,49 @@
-import {FunctionComponent} from 'react'
+import type { FunctionComponent, SVGAttributes } from 'react'
 
-export const BeatLoading : FunctionComponent; 
-export const BounceLoading : FunctionComponent; 
-export const CircularLoading : FunctionComponent; 
-export const ClockLoading : FunctionComponent; 
-export const RotateLoading : FunctionComponent; 
-export const SpinLoading : FunctionComponent; 
-export const WaveLoading : FunctionComponent; 
-export const DashLoading : FunctionComponent; 
-export const CopperLoading : FunctionComponent; 
+export const BeatLoading: FunctionComponent<{
+  duration?: number, 
+  count?: number, 
+  fill?: string, 
+  size?: number, 
+  gap?: number
+} & SVGAttributes<SVGAElement>>;
+export const BounceLoading: FunctionComponent<{
+  duration?: number,
+  count?: number,
+  barWidth?: number, 
+  barHeight?: number, 
+  gap?: number
+} & SVGAttributes<SVGAElement>>;
+export const CircularLoading: FunctionComponent<{
+  duration?: number, 
+  size?: number,
+} & SVGAttributes<SVGAElement>>;
+export const ClockLoading: FunctionComponent<{
+  size?: number, 
+  duration?: number,
+} & SVGAttributes<SVGAElement>>;
+export const RotateLoading: FunctionComponent<{
+  size?: number, 
+  duration?: number, 
+  opacity?: number,
+} & SVGAttributes<SVGAElement>>;
+export const SpinLoading: FunctionComponent<{
+  size?: number, 
+  barWidth?: number, 
+  barHeight?: number, 
+  count?: number,
+  duration?: number, fill, borderRadius
+} & SVGAttributes<SVGAElement>>;
+export const WaveLoading: FunctionComponent<{
+  size?: number, 
+  count?: number, 
+  duration?: number,
+} & SVGAttributes<SVGAElement>>;
+export const DashLoading: FunctionComponent<{
+  size?: number, 
+  duration?: number,
+} & SVGAttributes<SVGAElement>>;
+export const CopperLoading: FunctionComponent<{
+  size?: number,
+  duration?: number
+} & SVGAttributes<SVGAElement>>;
