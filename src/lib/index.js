28bf11cd0741a63591b8cf55e05e82329c4cc576
Jiachi Liu <inbox@huozhi.im>
2022-09-20 18:04:48 +0200

    remove unused

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,12 +2,6 @@ export const uniqId = '__respinner__'
 
 export const repeat = (count) => Array.apply(null, Array(count))
 
-const baseRotateAnimation = `Rotate${uniqId}{100%{transform:rotate(360deg)}}`
-export const keyFrame = '@keyframes'
-export const webkitKeyFrame = '@-webkit-keyframes'
-
 export const createCompatibleAnimation = (animation) => {
-  return `${webkitKeyFrame} ${animation.replace(/transform:/g, '-webkit-transform:')}; ${keyFrame} ${animation};`
+  return `@keyframes ${animation};`
 }
-
-export const RotateAnimation = createCompatibleAnimation(baseRotateAnimation)
